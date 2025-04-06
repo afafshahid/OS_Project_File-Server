d9b463f5ed06625c4cbc72d0e1e75a116845f73c
TahaNauman <mtahanauman90@gmail.com>
2025-04-06 07:40:51 +0500

    Authentication Header File

---
+++ auth.h
@@ -0,0 +1,10 @@
+// auth.h
+#ifndef AUTH_H
+#define AUTH_H
+
+void create_user(const char *username, const char *password);
+int verify_user(const char *username, const char *password);
+
+#endif
+
+
