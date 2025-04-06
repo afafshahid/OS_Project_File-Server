3d1d7ecdbedad9035ec9d6b1bcff4e9592f044a8
TahaNauman <mtahanauman90@gmail.com>
2025-04-06 07:44:40 +0500

    Email Header File

---
+++ email.h
@@ -0,0 +1,18 @@
+#ifndef EMAIL_H
+#define EMAIL_H
+
+#define FILE_NAME "emails.txt"
+
+typedef struct {
+    char sender[100];
+    char receiver[100];
+    char subject[200];
+    char body[500];
+    char folder[50];  // Inbox, Sent, Trash
+} Email;
+
+// Changed function name here
+void send_email(Email email);  // Previously store_email//
+int fetch_emails(const char *folder, Email *emails); 
+
+#endif
